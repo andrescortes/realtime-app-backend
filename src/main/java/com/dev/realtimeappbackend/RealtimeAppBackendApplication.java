eb921cafba46f905936e98a2c17b3587070718cc
Miguel Andres Cortes Reyes <andresabyac@gmail.com>
2022-10-17 07:06:16 -0500

    project base init

---
+++ src/main/java/com/dev/realtimeappbackend/RealtimeAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dev.realtimeappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/realtimeappbackend/RealtimeAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.realtimeappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
